34232eb6aa7902cc52e4a1c6f4462fcb393832fa
Al Danial <al.danial@gmail.com>
2016-11-04 09:09:37 -0700

    Python example

---
+++ tests/inputs/hi.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+
+# pound comment
+
+import time
+print('Hello.  The time is %f Unix epoch.' % (time.time()))  # inline comment
+
+"""
+Docstring,
+  also counted as comment.
+"""
