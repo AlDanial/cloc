cbf61f98c5cfddba41e6a82ea7973b3fdf35d59e
Al Danial <al.danial@gmail.com>
2016-11-04 15:17:03 -0700

    test cases, doc update for issue #131

---
+++ tests/inputs/issues/131/hi.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+# Works with both 2.x and 3.x versions of Python.
+
+# pound comment
+
+import time
+print('Hello.  The time is %f Unix epoch.' % (time.time()))  # inline comment
+
+"""
+Docstring,
+  also counted as comment.
+"""
